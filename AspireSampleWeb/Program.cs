15ff583b8a0910377e1dfd90e7d26ce9260e3d88
Georg Müller <georg@zyrr.io>
2025-02-05 10:14:50 +0100

    Add service defaults

--- AspireSampleWeb/Program.cs
+++ AspireSampleWeb/Program.cs
@@ -5,8 +5,7 @@ using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.AddServiceDefaults();
 builder.AddCshtmlComponents();
 builder.Services.AddRazorPages();
 
@@ -24,19 +23,16 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<CrmContext>((sp, options) => options
     .UseNpgsql(GetConnectionString(sp), dbContextOptions =>
         dbContextOptions
-            .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
-            .EnableRetryOnFailure()));
+            .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
 #endregion
 
 builder.Services.AddHostedService<CreateDatabaseBackgroundService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
