e379d46fe855eb2c96875bc7904a843ea68f9ae5
Georg Müller <georg@zyrr.io>
2025-02-10 18:10:56 +0100

    fix: Sample 4 and css

--- Prepared/04/Postgres/Program.cs
+++ Prepared/04/Postgres/Program.cs
@@ -5,13 +5,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 var dbPassword = builder.AddParameter("dbpwd", "DbPassword", secret: true);
 
 var postgresDb = builder.AddPostgres("db", password: dbPassword, port: 15432);
+var sampleDb = postgresDb.AddDatabase("sampledb", "sample");
 
 var smtp = builder.AddContainer("smtp", "docker.io/mailhog/mailhog")
     .WithEndpoint(name: "smtp-endpoint", port: 1025, targetPort: 1025)
     .WithHttpEndpoint(8025, 8025);
 
 builder.AddProject<AspireSampleWeb>("sample")
-    .WithReference(postgresDb)
+    .WithReference(sampleDb)
     .WithReference(smtp.GetEndpoint("smtp-endpoint"));
 
 builder.Build().Run();
